3ede2480dd4c5f2e329295f38b13380fde149028
Quang6666 <quangtrantroi@gmail.com>
2025-06-23 22:13:33 +0700

    Setup

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const client = require('./db');
 const path = require('path');
-const authRoutes = require('./routes/auth');
+const authRoutes = require('./2_routes/auth');
 
 const app = express();
 const PORT = 3000;
