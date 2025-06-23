ccf2ff751b09d4566e3249b4cb23ca943a050f63
Quang6666 <quangtrantroi@gmail.com>
2025-06-23 23:18:49 +0700

    update CSS for login page styling

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../3_controllers/authController');
+const authController = require('../controllers/authController');
 
 // API login
 router.post('/login', authController.loginApi);
